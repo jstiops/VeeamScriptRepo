cf78e4d5d0221c573d082010f38aebcee3f7972b
Jeffrey <jstrik@gmail.com>
2017-02-17 21:55:47 +0100

    Create Get-BackupJobFullSizeAndDay.ps1

---
+++ Get-BackupJobFullSizeAndDay.ps1
@@ -0,0 +1,1 @@
+Get-VBRJob | select @{N="Job Name";E={$_.Name}}, @{N="Size";E={[math]::Round(((Get-VBRBackup -name ($_.Name)).GetAllStorages().Stats.BackupSize | Measure-Object -sum).Sum/1gb,1)}}, @{N="Full Backup Days";E={$_.BackupTargetOptions.FullBackupDays}} | Sort-Object -Property "Job Name" 
