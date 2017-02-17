fd885147d53272ca24b323fbe2c0480d16678fcf
Jeffrey <jstrik@gmail.com>
2017-02-17 22:47:55 +0100

    Update Get-BackupJobBackupSizes.ps1

--- Get-BackupJobBackupSizes.ps1
+++ Get-BackupJobBackupSizes.ps1
@@ -1,2 +1,5 @@
 #display the size that backup file occupies on a repository for every job together with the VM data size
+if ((Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue) -eq $null) {
+    Add-PsSnapin -Name VeeamPSSnapIn
+}
 Get-VBRBackup | Select @{N="Job Name";E={$_.Name}}, @{N="VM Data Size (GB)";E={[math]::Round(($_.GetAllStorages().Stats.BackupSize | Measure-Object -Sum).Sum/1GB,1)}},@{N="Backupsize on Repo (GB)";E={[math]::Round(($_.GetAllStorages().Stats.datasize | Measure-Object -Sum).Sum/1GB,1)}} | Sort-Object -Property "Job Name" | Format-Table -AutoSize
