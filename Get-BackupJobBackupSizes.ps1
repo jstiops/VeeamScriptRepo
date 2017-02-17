40125b6b88d31fa211258fd584ef61eb2b9dfb2e
Jeffrey <jstrik@gmail.com>
2017-02-17 22:45:19 +0100

    Update Get-BackupJobBackupSizes.ps1

--- Get-BackupJobBackupSizes.ps1
+++ Get-BackupJobBackupSizes.ps1
@@ -1,2 +1,2 @@
-#display the size that backup file occupies on a repository for every job.
-Get-VBRBackup | Select @{N="Job Name";E={$_.Name}}, @{N="Size (GB)";E={[math]::Round(($_.GetAllStorages().Stats.BackupSize | Measure-Object -Sum).Sum/1GB,1)}} | Format-Table -AutoSize
+#display the size that backup file occupies on a repository for every job together with the VM data size
+Get-VBRBackup | Select @{N="Job Name";E={$_.Name}}, @{N="VM Data Size (GB)";E={[math]::Round(($_.GetAllStorages().Stats.BackupSize | Measure-Object -Sum).Sum/1GB,1)}},@{N="Backupsize on Repo (GB)";E={[math]::Round(($_.GetAllStorages().Stats.datasize | Measure-Object -Sum).Sum/1GB,1)}} | Sort-Object -Property "Job Name" | Format-Table -AutoSize
