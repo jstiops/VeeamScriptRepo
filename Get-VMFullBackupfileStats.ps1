3f5569611d2fc558706502de08c886e18d4e1cfe
Jeffrey <jstrik@gmail.com>
2017-02-24 21:45:42 +0100

    Update Get-VMFullBackupfileStats.ps1

--- Get-VMFullBackupfileStats.ps1
+++ Get-VMFullBackupfileStats.ps1
@@ -1,5 +1,6 @@
+# display full vbk file stats for specific VM
 if ((Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue) -eq $null) {
     Add-PsSnapin -Name VeeamPSSnapIn
 }
-
-Get-VBRRestorePoint | Where-Object {$_.IsFull -eq $true -and $_.vmname -eq "putVmnameHere"} | ForEach-Object { Write-Host $_.creationtime; write-host "Backupsizes:"; [math]::truncate($_.GetStorage().stats.Datasize / 1GB) ; write-host "Datasizes:" ; [math]::truncate($_.GetStorage().stats.Datasize / 1GB)  ; Write-host "=========="}
+$vmname = "putVMNameHere"
+Get-VBRRestorePoint | Where-Object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | ForEach-Object { Write-Host $_.creationtime; write-host "Backupsizes:"; [math]::truncate($_.GetStorage().stats.Datasize / 1GB) ; write-host "Datasizes:" ; [math]::truncate($_.GetStorage().stats.Datasize / 1GB)  ; Write-host "=========="}
