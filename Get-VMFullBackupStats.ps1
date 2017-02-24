f7f82ea76d1746e4b0597d1e60043aa0b074dd60
Jeffrey <jstrik@gmail.com>
2017-02-24 15:16:32 +0100

    Update Get-VMFullBackupStats.ps1

--- Get-VMFullBackupStats.ps1
+++ Get-VMFullBackupStats.ps1
@@ -1,4 +1,4 @@
 if ((Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue) -eq $null) {
     Add-PsSnapin -Name VeeamPSSnapIn
 }
-Get-VBRRestorePoint | Where-Object {$_.IsFull -eq $true -and $_.vmname -eq "insertVMName"}
+Get-VBRRestorePoint | Where-Object {$_.IsFull -eq $true -and $_.vmname -eq "svr-sqlbackup-001"} | ForEach-Object { write-host "Backupsizes:"; $_.GetStorage().stats.Backupsize / 1GB ; write-host "Datasizes:" ; $_.GetStorage().stats.Datasize / 1GB}
