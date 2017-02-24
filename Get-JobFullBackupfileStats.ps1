b9a131a546ef065668f70fc9e4d973de01144528
Jeffrey <jstrik@gmail.com>
2017-02-24 16:02:52 +0100

    Update Get-JobFullBackupfileStats.ps1

--- Get-JobFullBackupfileStats.ps1
+++ Get-JobFullBackupfileStats.ps1
@@ -1,5 +1,6 @@
 if ((Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue) -eq $null) {
     Add-PsSnapin -Name VeeamPSSnapIn
 }
-$vbkfiles = Get-VBRbackup -name "Backup Job 24Hour RPO Citrix"  | ForEach-Object {$_.getallstorages()} | Where-Object {$_.isfull -eq $true -and $_.isAvailable -eq $true} |select creationtime,Filepath,stats
+$jobname = "put in jobname"
+$vbkfiles = Get-VBRbackup -name $jobname | ForEach-Object {$_.getallstorages()} | Where-Object {$_.isfull -eq $true -and $_.isAvailable -eq $true} |select creationtime,Filepath,stats
 $vbkfiles | Sort-Object -Property Filepath,CreationTime | select Filepath,stats | % {write-host $_.filepath ; write-host "Datasize:" ([math]::truncate($_.stats.datasize /1gb)) ; write-host "Backupsize:" ([math]::truncate($_.stats.backupsize /1gb)) }
