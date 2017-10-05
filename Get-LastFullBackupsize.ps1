df44d831f08a3b254ece6aa6da96f4d2b27abb06
Jeffrey <jstrik@gmail.com>
2017-10-05 12:44:45 +0200

    Update Get-LastFullBackupsize.ps1

--- Get-LastFullBackupsize.ps1
+++ Get-LastFullBackupsize.ps1
@@ -1,11 +1,15 @@
 
 $job = get-vbrbackup -Name "jobname"
+$getDatasize = $false
 $vmList = ($job | Select @{n="vm";e={$_.GetObjectOibsAll() | %{@($_.name,"")}}} | Select -ExpandProperty vm);
 [int]$sum = 0
 foreach ($vmname in $vmlist){
-#$lastFullDataSize = get-vbrrestorepoint | where-object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | select -last 1 | % {[math]::truncate($_.GetStorage().stats.Datasize / 1GB)}
-$lastFullBackupSize = = get-vbrrestorepoint | where-object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | select -last 1 | % {[math]::truncate($_.GetStorage().stats.Backupsize / 1GB)}
-$sum += [int]$lastFullBackupSize
-}
-$sum
 
+if($getDatasize -eq &true){
+  $lastFullSize = get-vbrrestorepoint | where-object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | select -last 1 | % {[math]::truncate($_.GetStorage().stats.Datasize / 1GB)}
+}else{
+  $lastFullSize = get-vbrrestorepoint | where-object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | select -last 1 | % {[math]::truncate($_.GetStorage().stats.Backupsize / 1GB)}
+}
+$sum += [int]$lastFullSize
+}
+Write-host "Total Full backup size(GB): " $sum
