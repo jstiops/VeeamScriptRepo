ef4ab14f59bbf625943f70f037e20e7d37bf70a1
Jeffrey <jstrik@gmail.com>
2017-10-05 12:35:38 +0200

    Update Get-LastFullBackupsize.ps1

--- Get-LastFullBackupsize.ps1
+++ Get-LastFullBackupsize.ps1
@@ -3,8 +3,9 @@ $job = get-vbrbackup -Name "jobname"
 $vmList = ($job | Select @{n="vm";e={$_.GetObjectOibsAll() | %{@($_.name,"")}}} | Select -ExpandProperty vm);
 [int]$sum = 0
 foreach ($vmname in $vmlist){
-$lastFullSize = get-vbrrestorepoint | where-object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | select -last 1 | % {[math]::truncate($_.GetStorage().stats.Datasize / 1GB)}
-$sum += [int]$lastFullSize
+#$lastFullDataSize = get-vbrrestorepoint | where-object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | select -last 1 | % {[math]::truncate($_.GetStorage().stats.Datasize / 1GB)}
+$lastFullBackupSize = = get-vbrrestorepoint | where-object {$_.IsFull -eq $true -and $_.vmname -eq $vmname} | select -last 1 | % {[math]::truncate($_.GetStorage().stats.Backupsize / 1GB)}
+$sum += [int]$lastFullBackupSize
 }
 $sum
 
