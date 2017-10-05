ca5a93a81e70d2697bc8627339e48e58bc076e7f
Jeffrey <jstrik@gmail.com>
2017-10-05 15:51:57 +0200

    Update Get-LastFullBackupsize.ps1

--- Get-LastFullBackupsize.ps1
+++ Get-LastFullBackupsize.ps1
@@ -1,4 +1,6 @@
 #get the latest Full Backupsize or Datasize for a specific job.
+#using suggestions from https://forums.veeam.com/powershell-f26/list-vm-s-which-are-backed-up-by-job-t39525.html
+
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$True,Position=1)]
@@ -10,9 +12,28 @@ if ((Get-PSSnapin -Name VeeamPSSnapIn -ErrorAction SilentlyContinue) -eq $null) 
 
 }
 
-$getDatasize = $true
 $jobObject = Get-VBRJob -Name $jobname
-$vmList = $jobObject.GetObjectsInJob().name
+$vms_in_jobs = new-object System.Collections.ArrayList
+$allsessions = get-vbrbackupsession
+
+foreach ($job in $jobObject){
+    echo $job.name
+    $vms_protected_by_job = $null
+    $vms_protected_by_job = ($allsessions | where {($_.jobname -like $job.Name) -and ($_.name -notlike "*Retry*") } | sort-object CreationTimeUTC -Descending)[0] | get-vbrtasksession
+    foreach ($vm_protected_by_job in $vms_protected_by_job){
+        $vm_in_job = New-Object PSObject
+        Add-Member -InputObject $vm_in_job -MemberType NoteProperty -Name Name -Value $vm_protected_by_job.name
+        Add-Member -InputObject $vm_in_job -MemberType NoteProperty -Name JobName -value $vm_protected_by_job.jobname
+        $vms_in_jobs.add($vm_in_job) | out-null
+    }
+   
+}
+
+$vms_in_jobs_compare1 = $vms_in_jobs.name
+$vms_in_jobs_compare2 = $vms_in_jobs_compare1 | select -unique
+
+Compare-Object $vms_in_jobs_compare1 $vms_in_jobs_compare2
+$vmlist = $vms_in_jobs_compare2
 $vmCount = $vmList.count
 
 Write-host "Getting VM statistics for $vmCount VMs. Please wait...."
@@ -32,5 +53,6 @@ $reducedByFactor = [math]::Round(($sumDatasize / $sumBackupsize),2)
 Write-host "Total Full (uncompressed) datasize(GB): " $sumDatasize
 Write-host "Total Full (compressed) backupsize(GB): " $sumBackupsize
 Write-host "Reduced by $reducedByPercentage % or factor $reducedByFactor"
+
 
 
